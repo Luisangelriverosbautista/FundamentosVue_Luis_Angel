2d25a31ae0fc64fa513329bfbfdb8ec835ff14c7
Luis_Ang <158246858+Luisangelriverosbautista@users.noreply.github.com>
2025-03-17 19:57:52 -0600

    comitt 1 hola mundo xd

---
+++ fundamentosBasicos/src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="container">
+  <h1> HOLA SOY LUIS</h1>
+
+  <P class="parrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Praesentium hic minus beatae recusandae vel optio,
+     laudantium exercitationem culpa accusantium quia omnis fuga laborum eum consectetur. 
+     Dignissimos reprehenderit exercitationem numquam quas?
+  </P></div>
+</template>
+
+<style scoped>
+.parrafo{
+  text-align: center;
+  color: red;
+}
+.read-the-docs {
+  color: #888;
+}
+</style>
